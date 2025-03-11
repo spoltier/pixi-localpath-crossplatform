ef9f7a76cce083eac951b1a5a3b8d0ef088ef652
Simon Poltier <simon.poltier@mirai-solutions.com>
2025-03-11 12:01:25 +0100

    provide prefilled repodata json

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,7 @@
-New-Item -Path "C:\tmp\custom_artifacts\conda\win-64" -ItemType Directory -Force
-New-Item -Path "C:\tmp\custom_artifacts\conda\linux-64" -ItemType Directory -Force
-New-Item -Path "C:\tmp\custom_artifacts\conda\noarch" -ItemType Directory -Force
+New-Item -Path "C:\tmp\custom_artifacts" -ItemType Directory -Force
 
-# New-Item -Path "C:\tmp\custom_artifacts\conda\noarch\repodata.json" -ItemType File -Force
+Copy-Item -Path ".\conda" -Destination "C:\tmp\custom_artifacts\" -Recurse -Force
+
 
 Invoke-WebRequest -Uri "https://anaconda.org/conda-forge/hll/2.3.0/download/win-64/hll-2.3.0-py38h2698bfa_0.conda" -OutFile "C:\tmp\custom_artifacts\conda\win-64\hll-2.3.0-py38h2698bfa_0.conda"
 Invoke-WebRequest -Uri "https://anaconda.org/conda-forge/hll/2.3.0/download/linux-64/hll-2.3.0-py313h46c70d0_0.conda" -OutFile "C:\tmp\custom_artifacts\conda\linux-64\hll-2.3.0-py313h46c70d0_0.conda"
--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-mkdir -p /tmp/custom_artifacts/conda/win-64
-mkdir -p /tmp/custom_artifacts/conda/linux-64
-mkdir -p /tmp/custom_artifacts/conda/noarch
+mkdir -p /tmp/custom_artifacts
 
-echo {} > /tmp/custom_artifacts/conda/noarch/repodata.json
+cp -r conda /tmp/custom_artifacts/
 
-wget -P /tmp/custom_artifacts/conda/win-64 https://anaconda.org/conda-forge/hll/2.3.0/download/win-64/hll-2.3.0-py38h2698bfa_0.conda
-wget -P /tmp/custom_artifacts/conda/linux-64 https://anaconda.org/conda-forge/hll/2.3.0/download/linux-64/hll-2.3.0-py313h46c70d0_0.conda
+WIN_FILE="/tmp/custom_artifacts/conda/win-64/hll-2.3.0-py38h2698bfa_0.conda"
+LINUX_FILE="/tmp/custom_artifacts/conda/linux-64/hll-2.3.0-py313h46c70d0_0.conda"
 
+if [ ! -f "$WIN_FILE" ]; then
+    wget -P /tmp/custom_artifacts/conda/win-64 https://anaconda.org/conda-forge/hll/2.3.0/download/win-64/hll-2.3.0-py38h2698bfa_0.conda
+fi
+
+if [ ! -f "$LINUX_FILE" ]; then
+    wget -P /tmp/custom_artifacts/conda/linux-64 https://anaconda.org/conda-forge/hll/2.3.0/download/linux-64/hll-2.3.0-py313h46c70d0_0.conda
+fi
