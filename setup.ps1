b26b9b28dca7079e32223f7f2968bc7279d675f4
Simon Poltier <simon.poltier@mirai-solutions.com>
2025-03-11 11:14:16 +0100

    must bootstrap before any pixi project commands

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,10 @@
-New-Item -Path "C:\tmp\private_artifacts\conda\win-64" -ItemType Directory -Force
-New-Item -Path "C:\tmp\private_artifacts\conda\linux-64" -ItemType Directory -Force
-New-Item -Path "C:\tmp\private_artifacts\conda\noarch" -ItemType Directory -Force
+New-Item -Path "C:\tmp\custom_artifacts\conda\win-64" -ItemType Directory -Force
+New-Item -Path "C:\tmp\custom_artifacts\conda\linux-64" -ItemType Directory -Force
+New-Item -Path "C:\tmp\custom_artifacts\conda\noarch" -ItemType Directory -Force
 
-New-Item -Path "C:\tmp\private_artifacts\conda\noarch\repodata.json" -ItemType File -Force
+# New-Item -Path "C:\tmp\custom_artifacts\conda\noarch\repodata.json" -ItemType File -Force
 
-Invoke-WebRequest -Uri "https://anaconda.org/conda-forge/hll/2.3.0/download/win-64/hll-2.3.0-py38h2698bfa_0.conda" -OutFile "C:\tmp\private_artifacts\conda\win-64\hll-2.3.0-py38h2698bfa_0.conda"
-Invoke-WebRequest -Uri "https://anaconda.org/conda-forge/hll/2.3.0/download/linux-64/hll-2.3.0-py313h46c70d0_0.conda" -OutFile "C:\tmp\private_artifacts\conda\linux-64\hll-2.3.0-py313h46c70d0_0.conda"
+Invoke-WebRequest -Uri "https://anaconda.org/conda-forge/hll/2.3.0/download/win-64/hll-2.3.0-py38h2698bfa_0.conda" -OutFile "C:\tmp\custom_artifacts\conda\win-64\hll-2.3.0-py38h2698bfa_0.conda"
+Invoke-WebRequest -Uri "https://anaconda.org/conda-forge/hll/2.3.0/download/linux-64/hll-2.3.0-py313h46c70d0_0.conda" -OutFile "C:\tmp\custom_artifacts\conda\linux-64\hll-2.3.0-py313h46c70d0_0.conda"
 
-conda index "C:\tmp\private_artifacts\conda\"
+conda index "C:\tmp\custom_artifacts\conda\"
